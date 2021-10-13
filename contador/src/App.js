75a29de512fe655c73adfceae9703a615dccb201
natizap <natalia.vilela01@etec.sp.gov.br>
2021-10-13 19:36:45 +0000

    Aula 03 - Contador
    Signed-off-by: natizap <natalia.vilela01@etec.sp.gov.br>

---
+++ contador/src/App.js
@@ -0,0 +1,17 @@
+import './App.css';
+
+import React from 'react'
+
+import Contador from './components/Contador'
+import Card from './components/layout/Card'
+
+export default () => (
+  <div className="App">
+    <h1>Contador</h1>
+    <div className="Cards">
+      <Card titulo="Contador componentizado" color="#0d3024">
+        <Contador numeroIncial={10} />
+      </Card>
+    </div>
+  </div>
+);
